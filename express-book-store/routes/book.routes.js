b43a39c8bd13912eb9e2cc4a7fcfb182550bd427
Yash Karande <yashkarande4240@gmail.com>
2026-06-14 23:31:52 +0530

    book routes defined

---
+++ express-book-store/routes/book.routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { getAllBooks, getBookById, createBook, updateBookById, updateBookPartiallyById, deleteBookById } = require("../controllers/book.controller");
+
+
+const router = express.Router();
+
+router.get("/", getAllBooks);
+
+router.get("/:id", getBookById);
+
+router.post("/", createBook);
+
+
+router.put("/:id", updateBookById);
+
+
+router.patch("/:id", updateBookPartiallyById);
+
+router.delete("/:id", deleteBookById);

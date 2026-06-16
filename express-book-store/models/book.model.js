80880c292b64a927e4a43d8443e805db69ea9cb4
Yash Karande <yashkarande4240@gmail.com>
2026-06-16 23:27:23 +0530

    book model updated

--- express-book-store/models/book.model.js
+++ express-book-store/models/book.model.js
@@ -1,11 +1,30 @@
-const {pgTable, integer, varchar, text, uuid} = require("drizzle-orm/pg-core");
+const {
+  pgTable,
+  integer,
+  varchar,
+  text,
+  uuid,
+} = require("drizzle-orm/pg-core");
 const authorsTable = require("./author.model.js");
+const { index } = require("drizzle-orm/gel-core");
+const { sql } = require("drizzle-orm");
 
-const booksTable = pgTable("books", {
-  id: uuid().primaryKey().defaultRandom(),
-  title: varchar({length: 100}).notNull(),
-  description: text().notNull(),
-  authorId: uuid().references(() => authorsTable.id).notNull()
-});
+const booksTable = pgTable(
+  "books",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    title: varchar({ length: 100 }).notNull(),
+    description: text().notNull(),
+    authorId: uuid()
+      .references(() => authorsTable.id)
+      .notNull(),
+  },
+  (table) => ({
+    searchIndexOnTitle: index("title_index").using(
+      "gin",
+      sql`to_tsvector('english', ${table.title})`,
+    ),
+  }),
+);
 
 module.exports = booksTable;
